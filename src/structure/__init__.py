ce634e44a8923946285d5ba638e0f56d4cd48d05
Ariel Alon <arielalon.12321@gmail.com>
2025-05-30 01:18:31 +0300

    Export library globals for simpler imports

---
+++ src/structure/__init__.py
@@ -0,0 +1,6 @@
+from structure._structure import Structure
+
+from structure.codecs.integer import IntegerCodec, Sign
+
+from structure.endianes import Endianes
+from structure.bit_buffer import BitBuffer
