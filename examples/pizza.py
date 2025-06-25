779b016888359c2f43b00a5937a90a37fc6bd030
Ariel Alon <arielalon.12321@gmail.com>
2025-06-25 19:06:43 +0300

    examples: Add a new example - pizza.py

---
+++ examples/pizza.py
@@ -0,0 +1,54 @@
+from enum import auto
+from typing import Annotated, List
+
+from bytex import Structure, StructureEnum
+from bytex.endianes import Endianes
+from bytex.length_encodings import Terminator, Prefix
+from bytex.types import U8, U16
+
+
+class OrderType(StructureEnum(U8)):
+    DINE_IN = auto()
+    DELIVERY = auto()
+    PICKUP = auto()
+
+
+class Topping(Structure):
+    name: Annotated[str, Terminator("\0")]
+
+
+class Pizza(Structure):
+    pizza_id: U16
+    toppings: Annotated[List[Topping], Prefix(U8)]
+
+
+class PizzaOrder(Structure):
+    order_type: OrderType
+    customer_id: U16
+    pizzas: Annotated[List[Pizza], Prefix(U8)]
+
+
+def main() -> None:
+    order = PizzaOrder(
+        order_type=OrderType.DELIVERY,
+        customer_id=1234,
+        pizzas=[
+            Pizza(pizza_id=1, toppings=[Topping(name="Onions")]),
+            Pizza(
+                pizza_id=2,
+                toppings=[Topping(name="Extra Cheese"), Topping(name="Mushrooms")],
+            ),
+        ],
+    )
+
+    print(order)
+
+    print(f"Order Dumped: {order.dump(endianes=Endianes.BIG)}")
+
+    roundtrip = PizzaOrder.parse(order.dump())
+
+    assert roundtrip.pizzas[0].toppings[0].name == "Onions"
+
+
+if __name__ == "__main__":
+    main()
