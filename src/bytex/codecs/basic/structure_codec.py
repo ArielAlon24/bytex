15215c8066f983b124d5e52109be25e7a94449d8
Ariel Alon <arielalon.12321@gmail.com>
2025-09-05 17:35:12 +0300

    Support new codec API in structure codec

--- src/bytex/codecs/basic/structure_codec.py
+++ src/bytex/codecs/basic/structure_codec.py
@@ -1,21 +1,22 @@
 from dataclasses import dataclass
 from typing import Type
 
-from bytex.structure._structure import _Structure
 from bytex.bits import BitBuffer, Bits
 from bytex.codecs.base_codec import BaseCodec
+from bytex.endianes import Endianes
 from bytex.errors import ValidationError
+from bytex.structure._structure import _Structure
 
 
 @dataclass(frozen=True)
 class StructureCodec(BaseCodec[_Structure]):
     structure_class: Type[_Structure]
 
-    def serialize(self, value: _Structure) -> Bits:
-        return value.dump_bits()
+    def serialize(self, value: _Structure, endianes: Endianes) -> Bits:
+        return value.dump_bits(endianes=endianes)
 
-    def deserialize(self, bit_buffer: BitBuffer) -> _Structure:
-        return self.structure_class.parse_bits(bit_buffer)
+    def deserialize(self, bit_buffer: BitBuffer, endianes: Endianes) -> _Structure:
+        return self.structure_class.parse_bits(bit_buffer, endianes=endianes)
 
     def validate(self, value: _Structure) -> None:
         if not isinstance(value, _Structure):
