d861e05b731eedc7c9c7862b3492d9a3bf632018
Ariel Alon <arielalon.12321@gmail.com>
2025-09-05 17:34:56 +0300

    Support new codec API in enum codec

--- src/bytex/codecs/enum_codec.py
+++ src/bytex/codecs/enum_codec.py
@@ -3,6 +3,7 @@ from typing import Type
 
 from bytex.bits import BitBuffer, Bits
 from bytex.codecs.base_codec import BaseCodec
+from bytex.endianes import Endianes
 from bytex.errors import ParsingError, ValidationError
 from bytex.structure_enum import _StructureEnum
 
@@ -18,11 +19,12 @@ class EnumCodec(BaseCodec[_StructureEnum]):
                 f"Invalid value, a {self.__class__.__name__}'s value must be of type '{str(_StructureEnum)}'"
             )
 
-    def serialize(self, value: _StructureEnum) -> Bits:
-        return self.item_codec.serialize(value.value)
+    def serialize(self, value: _StructureEnum, endianes: Endianes) -> Bits:
+        return self.item_codec.serialize(value.value, endianes=endianes)
 
-    def deserialize(self, bit_buffer: BitBuffer) -> _StructureEnum:
-        value = self.item_codec.deserialize(bit_buffer)
+    def deserialize(self, bit_buffer: BitBuffer, endianes: Endianes) -> _StructureEnum:
+        value = self.item_codec.deserialize(bit_buffer, endianes=endianes)
+
         try:
             return self.enum(value)
         except ValueError as e:
