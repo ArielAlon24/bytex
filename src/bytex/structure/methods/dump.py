ccf12d431fa6801cc5fc13854077f28023a8e0a2
Ariel Alon <arielalon.12321@gmail.com>
2025-09-05 17:37:31 +0300

    Update dump methods endianes use

--- src/bytex/structure/methods/dump.py
+++ src/bytex/structure/methods/dump.py
@@ -1,9 +1,9 @@
 from typing import Callable
 
-from bytex.structure.types import Fields
-from bytex.endianes import Endianes
 from bytex.bits import BitBuffer
+from bytex.endianes import Endianes
 from bytex.errors import AlignmentError
+from bytex.structure.types import Fields
 
 
 def _create_dump(fields: Fields) -> Callable[[object, Endianes], bytes]:
@@ -11,10 +11,10 @@ def _create_dump(fields: Fields) -> Callable[[object, Endianes], bytes]:
         buffer = BitBuffer()
         for name, field in fields.items():
             value = getattr(self, name)
-            buffer.write(field.codec.serialize(value))
+            buffer.write(field.codec.serialize(value, endianes=endianes))
 
         try:
-            return buffer.to_bytes(endianes=endianes)
+            return buffer.to_bytes()
         except AlignmentError as e:
             raise AlignmentError(
                 "Cannot dump a structure whose bit size is not a multiple of 8"
--- src/bytex/structure/methods/dump_bits.py
+++ src/bytex/structure/methods/dump_bits.py
@@ -1,16 +1,17 @@
 from typing import Callable
 
+from bytex.bits import BitBuffer, Bits
+from bytex.endianes import Endianes
 from bytex.structure.types import Fields
-from bytex.bits import Bits, BitBuffer
 
 
-def _create_dump_bits(fields: Fields) -> Callable[[object], Bits]:
-    def dump_bits(self) -> Bits:
+def _create_dump_bits(fields: Fields) -> Callable[[object, Endianes], Bits]:
+    def dump_bits(self, endianes: Endianes) -> Bits:
         buffer = BitBuffer()
 
         for name, field in fields.items():
             value = getattr(self, name)
-            buffer.write(field.codec.serialize(value))
+            buffer.write(field.codec.serialize(value, endianes=endianes))
 
         return buffer.to_bits()
 
